67d830a3c59bda215af1f31292c781126b1e2c42
ranbee420 <maximjosan07@gmail.com>
2020-08-04 21:25:02 +0300

    help

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -5,8 +7,14 @@ module.exports = {
 		whatever();
 		async function whatever() {
 			try {
-				await message.author.send('Here\'s a list of my commands:')
-				await message.author.send('Prefix: e!\n**help** (e!help)\n**kick** (e!kick [Mention])\n**ban** (e!ban [Mention])\n**math** (e!math 1 1)\n**myav** (e!myav)\n**online** (e!online)\n**ping** (e!ping)\n**play** (e!play [Youtube Link])\n**leave** (e!leave (Leaves VC))\n**poll** (e!poll [Text])\n**prune** (e!prune [1-99])\n**randomnum** (e!randomnum [Number])\n**roleadd** (e!roleadd [Existing Role])\n**rolecreate** (e!rolecreate [Text])\n**say** (e!say (Text))\n**server** (e!server)\n*Forbidden Commands:*\n**uwuify** (e!uwuify [Text])\n**uwuface** (e!uwuface)')
+				const embed = new Discord.MessageEmbed()
+					.setColor('RANDOM')
+					.setTitle('Help!')
+					.setAuthor('EssentialBot')
+					.setDescription('test')
+					.addField('test', 'test', true)
+					.setTimestamp()
+				message.author.send(embed);
 			if (!message.guild){
 				return;
 			} else {
