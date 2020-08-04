c78712e1754154a9cfd8ab989e5dc7d2dfabfbe8
ranbee420 <maximjosan07@gmail.com>
2020-08-04 20:44:31 +0300

    changes

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
 		async function whatever() {
 			try {
 				await message.author.send('Here\'s a list of my commands:')
-				await message.author.send('Prefix: e!\n**help** (e!help)\n**kick** (e!kick [Mention])\n**ban** (e!ban [Mention])\n**math** (e!math 1 1)\n**myav** (e!myav)\n**online** (e!online)\n**ping** (e!ping)\n**play** (e!play [Youtube Link])\n**leave** (e!leave (Leaves VC))\n**poll** (e!poll [Text])\n**prune (e!prune [1-99])**\n**randomnum (e!randomnum [Number])**\n**roleadd** (e!roleadd [Existing Role])\n**rolecreate** (e!rolecreate [Text])\n**say** (e!say (Text))\n**server** (e!server)\n*Forbidden Commands:*\n**uwuify** (e!uwuify [Text])\nuwuface (e!uwuface)')
+				await message.author.send('Prefix: e!\n**help** (e!help)\n**kick** (e!kick [Mention])\n**ban** (e!ban [Mention])\n**math** (e!math 1 1)\n**myav** (e!myav)\n**online** (e!online)\n**ping** (e!ping)\n**play** (e!play [Youtube Link])\n**leave** (e!leave (Leaves VC))\n**poll** (e!poll [Text])\n**prune** (e!prune [1-99])\n**randomnum** (e!randomnum [Number])\n**roleadd** (e!roleadd [Existing Role])\n**rolecreate** (e!rolecreate [Text])\n**say** (e!say (Text))\n**server** (e!server)\n*Forbidden Commands:*\n**uwuify** (e!uwuify [Text])\n**uwuface** (e!uwuface)')
 			if (!message.guild){
 				return;
 			} else {
---
+++ commands/jews.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'jews',
+	description: 'funny',
+	execute(message, args) {
+		message.channel.send('https://web.sascha-t.de/jews.mp4');
+	}
+}
