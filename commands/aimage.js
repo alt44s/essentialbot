6048e92f14c4ccb467606d9ef03003b5bb4e4021
ranbee420 <maximjosan07@gmail.com>
2020-08-11 23:45:51 +0300

    awof

--- commands/aimage.js
+++ commands/aimage.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var ai = require('ascii-images');
-const image = message.attachments;
 
 module.exports = {
 	name: 'aimage',
 	description: 'ascii to image',
 	execute(message, args) {
+        const image = message.attachments;
+
 		ai(image, function(result){
             message.channel.send(`\`\`\`${result}\`\`\``);
           });          
