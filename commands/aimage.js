af7628e3d7bcdff86b3700b08694f561f3ce9aff
ranbee420 <maximjosan07@gmail.com>
2020-08-11 23:59:13 +0300

    h

--- commands/aimage.js
+++ commands/aimage.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var ai = require('ascii-images');
+var imgur = require('imgur');
 
 module.exports = {
 	name: 'aimage',
@@ -8,7 +9,15 @@ module.exports = {
 	execute(message, args) {
         const image = message.attachments;
 
-		ai(image, function(result){
+        imgur.uploadFile(image)
+        .then(function (json) {
+          
+     })
+         .catch(function (err) {
+            console.error(err.message);
+     });
+
+		ai(json.data.link, function(result){
             message.channel.send(`\`\`\`${result}\`\`\``);
           });          
 	}
