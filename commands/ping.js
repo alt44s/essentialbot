1ef6df81b5077f691c1b5a6ce197499a14b1d12d
ranbee420 <maximjosan07@gmail.com>
2020-08-12 19:32:36 +0300

    wananvbe

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'fuck you',
 	execute(message, args) {
 		var ping = Date.now() - message.createdTimestamp + " ms";
-    message.channel.sendMessage("`" + `${Date.now() - message.createdTimestamp}` + " ms`");
+    message.channel.send("`" + `${Date.now() - message.createdTimestamp}` + " ms`");
 	}
 }
