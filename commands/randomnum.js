26d0e3444c0b3a04909af31928f9ccc82cc8a886
ranbee420 <maximjosan07@gmail.com>
2020-08-15 14:56:17 +0300

    dw

--- commands/randomnum.js
+++ commands/randomnum.js
@@ -5,12 +5,15 @@ module.exports = {
 	name: 'randomnum',
 	description: 'generates a random number',
 	execute(message) {
+
+		var number1 = args[0];
+		var number2 = args[1];
+
 		function getRandomInt(max) {
-		return Math.floor(Math.random() * Math.floor(max));
+		return Math.floor((Math.random() * number2) + number1);
 		}
 		
-		var number = message.content.substr(11)
-		message.channel.send(getRandomInt(number))
+		message.channel.send(getRandomInt);
 
 		if (isNaN(number)) {
 			message.channel.send('Thats an invalid number')
