44d7aa8b44a37813270633aca0de4079b49c3569
ranbee420 <maximjosan07@gmail.com>
2020-08-15 15:41:52 +0300

    dw

--- commands/randomnum.js
+++ commands/randomnum.js
@@ -3,14 +3,14 @@ const client = new Discord.Client();
 
 module.exports = {
 	name: 'randomnum',
-	description: 'generates a random number',
+	description: 'random number',
 	execute(message, args) {
 
 		var number1 = args[0];
 		var number2 = args[1];
 
 		function getRandomInt(max) {
-		return Math.floor((Math.random() * number2) + number1);
+			return Math.floor(Math.random() * (number2 - number1 + 1) ) + number1;
 		}
 		
 		const number = getRandomInt();
